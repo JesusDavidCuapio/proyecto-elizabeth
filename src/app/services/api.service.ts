6837ff9a8d5f8c0189a7c2c8cf8ecc21b850007c
JesusDavidCuapio <Paullennon2017@gmail.com>
2025-07-18 15:00:40 -0600

    cambios de .env y ruta a railway en api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'http://localhost:3000/api';
+private baseUrl = 'https://proyecto-elizabeth-backend-production.up.railway.app/api';
   
   constructor(private http: HttpClient) {}
 
